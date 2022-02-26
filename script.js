0f19964b45a8737e7fc4e151cd87d31a7c4a415b
Jason Felice <jasonfelice0@gmail.com>
2022-02-26 05:53:29 -0800

    Finalized the script and the style: > Computer button changes as according to computer selection. > Added a round selector feature that supports up to 10 rounds. > Fixed Game Over overlay and added a Play Again button. > For fun I added an animated rainbow underscore. > Added some shadows to overlay text and button(s)

--- script.js
+++ script.js
@@ -1,74 +1,111 @@
-function mainGame(e) {
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+let totalPlayerScores = 0;
+let totalComputerScores = 0;
+let selectedRounds = 3;
 
+document.getElementById("playAgain").addEventListener("click", playAgain);
+document.getElementById("roundPlus").addEventListener("click", roundPlus);
+document.getElementById("roundMinus").addEventListener("click", roundMinus);
+
+document.getElementById('roundDisplayBox').textContent = `${selectedRounds}`;
+
+
+
+
+function mainGame(e) {
+    document.getElementById("round_container").classList.remove("display_flex");
+    document.getElementById("round_container").classList.add("display_none");
     const playerScoreElement = document.createElement("h1");
     const computerScoreElement = document.createElement("h1");
     const playerSelection = e.target.attributes.value.nodeValue;
     const computerSelection = computerPlay();
     let winner = 0;
     round++;
+    whoWon();
+    updateComputerButtons();
+    updateScores();
+    gameOver();
 
-    if (playerSelection == "rock" && computerSelection == "rock") { winner = 0; }
-    else if (playerSelection == "rock" && computerSelection == "paper") { winner = -1; }
-    else if (playerSelection == "rock" && computerSelection == "scissors") { winner = 1; }
-
-    else if (playerSelection == "paper" && computerSelection == "rock") { winner = 1; }
-    else if (playerSelection == "paper" && computerSelection == "paper") { winner = 0; }
-    else if (playerSelection == "paper" && computerSelection == "scissors") { winner = -1; }
-
-    else if (playerSelection == "scissors" && computerSelection == "rock") { winner = -1; }
-    else if (playerSelection == "scissors" && computerSelection == "paper") { winner = 1; }
-    else if (playerSelection == "scissors" && computerSelection == "scissors") { winner = 0; }
-
-
-    if (winner == 1) {
-        playerScore++;
-        playerScoreElement.appendChild(document.createTextNode("1"));
-        computerScoreElement.appendChild(document.createTextNode("0"));
-        totalPlayerScores++;
+    function updateComputerButtons(){
+        if(computerSelection === "paper"){
+            displayPaper();
+            }
+        else if(computerSelection === "rock"){
+         displayRock();
+        }
+        else if(computerSelection === "scissors"){
+         displayScissors();
+        }
     }
-    else if (winner == -1) {
-        computerScore++;
-        playerScoreElement.appendChild(document.createTextNode("0"));
-        computerScoreElement.appendChild(document.createTextNode("1"));
-        totalComputerScores++;
+
+    function whoWon(){
+        if (playerSelection == "rock" && computerSelection == "rock") { winner = 0; }
+        else if (playerSelection == "rock" && computerSelection == "paper") { winner = -1; }
+        else if (playerSelection == "rock" && computerSelection == "scissors") { winner = 1; }
+
+        else if (playerSelection == "paper" && computerSelection == "rock") { winner = 1; }
+        else if (playerSelection == "paper" && computerSelection == "paper") { winner = 0; }
+        else if (playerSelection == "paper" && computerSelection == "scissors") { winner = -1; }
+
+        else if (playerSelection == "scissors" && computerSelection == "rock") { winner = -1; }
+        else if (playerSelection == "scissors" && computerSelection == "paper") { winner = 1; }
+        else if (playerSelection == "scissors" && computerSelection == "scissors") { winner = 0; }
     }
-    else {
-        playerScoreElement.appendChild(document.createTextNode("0"));
-        computerScoreElement.appendChild(document.createTextNode("0"));
+
+    function updateScores(){
+        if (winner == 1) {
+            playerScore++;
+            playerScoreElement.appendChild(document.createTextNode("1"));
+            computerScoreElement.appendChild(document.createTextNode("0"));
+            totalPlayerScores++;
+        }
+        else if (winner == -1) {
+            computerScore++;
+            playerScoreElement.appendChild(document.createTextNode("0"));
+            computerScoreElement.appendChild(document.createTextNode("1"));
+            totalComputerScores++;
+        }
+        else {
+            playerScoreElement.appendChild(document.createTextNode("0"));
+            computerScoreElement.appendChild(document.createTextNode("0"));
+        }
+        document.querySelector("#player_scores > .score").appendChild(playerScoreElement);
+        document.querySelector("#computer_scores > .score").appendChild(computerScoreElement);
     }
-    document.querySelector("#player_scores > .score").appendChild(playerScoreElement);
-    document.querySelector("#computer_scores > .score").appendChild(computerScoreElement);
 
-
-    if (round == 5) {
-        document.getElementById("totalPlayerScores").textContent = `Player: ${totalPlayerScores}`;
-        document.getElementById("totalComputerScores").textContent = `Computer: ${totalComputerScores}`;
-        document.getElementById("total_scores").classList.remove("display_none");
-        document.getElementById("total_scores").classList.add("display_flex");
-        console.log([playerScore, computerScore]);
-        console.log(round);
-        playerScore = 0;
-        computerScore = 0;
-        round = 0;
-        totalPlayerScores = 0;
-        totalComputerScores = 0;
-
+    function gameOver(){
+        if (round === selectedRounds) {
+            document.getElementById("gameOver").classList.remove("display_none");
+            document.getElementById("gameOver").classList.remove("display_block");
+            document.getElementById("totalPlayerScores").textContent = `Player: ${totalPlayerScores}`;
+            document.getElementById("totalComputerScores").textContent = `Computer: ${totalComputerScores}`;
+            document.getElementById("total_scores").classList.remove("display_none");
+            document.getElementById("total_scores").classList.add("display_flex");
+            document.getElementById("winOrLose").classList.remove("display_none");
+            document.getElementById("playAgain").classList.remove("display_none");
+        
+            if(totalPlayerScores>totalComputerScores){
+                document.getElementById('win').classList.remove('display_none');
+            }
+            else if(totalPlayerScores<totalComputerScores){
+                document.getElementById('lose').classList.remove('display_none');
+            }
+            else if(totalPlayerScores==totalComputerScores){
+                document.getElementById('tie').classList.remove('display_none');
+            }
+        for (let i = 0; i < document.querySelectorAll("#player_selection > .btns").length; i++) {
+            document.querySelectorAll("#player_selection > .btns")[i].removeEventListener("click", mainGame);
+            setTimeout(displayAllComputerButtons, 1500);
+        }
+        }
     }
 }
 
-let playerScore = 0;
-let computerScore = 0;
-let round = 0;
-let totalPlayerScores = 0;
-let totalComputerScores = 0;
 
-
-function clearScoreBoard() {
-    const length = document.querySelectorAll("#player_scores .score h1").length;
-    for (let i = 0; i < length; i++) {
-        document.querySelectorAll(".score")[0].removeChild(document.querySelectorAll("#player_scores .score h1")[0]);
-        document.querySelectorAll(".score")[1].removeChild(document.querySelectorAll("#computer_scores .score h1")[0]);
-    }
+function playAgain(){
+    location.reload();
 }
 
 function computerPlay() {
@@ -78,4 +115,58 @@ function computerPlay() {
 
 for (let i = 0; i < document.querySelectorAll("#player_selection > .btns").length; i++) {
     document.querySelectorAll("#player_selection > .btns")[i].addEventListener("click", mainGame);
+}
+for (let i = 0; i < document.querySelectorAll("#computer_selection > .btns").length; i++) {
+    document.querySelectorAll("#computer_selection > .btns")[i].addEventListener("click", computerHands);
+}
+
+function displayPaper(){
+    document.getElementById("paper_c").classList.remove("display_none");
+    document.getElementById("rock_c").classList.add("display_none");
+    document.getElementById("scissors_c").classList.add("display_none");
+}
+
+function displayRock(){
+    document.getElementById("rock_c").classList.remove("display_none");
+    document.getElementById("paper_c").classList.add("display_none");
+    document.getElementById("scissors_c").classList.add("display_none");
+}
+
+function displayScissors(){
+    document.getElementById("scissors_c").classList.remove("display_none");
+    document.getElementById("paper_c").classList.add("display_none");
+    document.getElementById("rock_c").classList.add("display_none");
+}
+
+function displayAllComputerButtons(){
+    document.getElementById("scissors_c").classList.remove("display_none");
+    document.getElementById("paper_c").classList.remove("display_none");
+    document.getElementById("rock_c").classList.remove("display_none");
+}
+
+function roundPlus(){
+    selectedRounds++;
+    document.getElementById('roundDisplayBox').textContent = `${selectedRounds}`;
+    if(selectedRounds===10){
+        document.getElementById("roundPlus").removeEventListener("click", roundPlus);
+    }
+    else if(selectedRounds>1){
+        document.getElementById("roundMinus").addEventListener("click", roundMinus);
+    }
+}
+
+function roundMinus(){
+    selectedRounds--;
+    document.getElementById('roundDisplayBox').textContent = `${selectedRounds}`;
+    if(selectedRounds===1){
+        document.getElementById("roundMinus").removeEventListener("click", roundMinus);
+    }
+    else if(selectedRounds<10){
+        document.getElementById("roundPlus").addEventListener("click", roundPlus);
+    }
+}
+
+function computerHands(){
+    const choices = ["Use your hands! The ones the left side.", "These are my hands!", "Not your hands!"]
+    alert(choices[Math.floor(Math.random() * choices.length)]);
 }
